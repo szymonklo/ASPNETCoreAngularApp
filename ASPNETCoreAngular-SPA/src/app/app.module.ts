89ce8b6f7db115b9b27bcb6f96096de0856103b0
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-04-02 21:38:16 +0200

    Added components (bootswatch, ngx bootstrap etc)

--- ASPNETCoreAngular-SPA/src/app/app.module.ts
+++ ASPNETCoreAngular-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -20,7 +22,9 @@ import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.in
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       ErrorInterceptorProvider,
