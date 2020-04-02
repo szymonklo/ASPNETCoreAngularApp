2060df0aad422341f0a5d2bde2e14eb9cdd97bdc
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-04-02 09:50:43 +0200

    Initial with Login

---
+++ ASPNETCoreAngular.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using ASPNETCoreAngular.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreAngular.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Value> Values {get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
