f51d056f0bc44700a9e04f9ecebc7be36381b9cc
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-04-04 07:15:27 +0200

    Updating user info

--- ASPNETCoreAngular.API/Helpers/AutoMapperProfiles.cs
+++ ASPNETCoreAngular.API/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace ASPNETCoreAngular.API.Helpers
                 .ForMember(dest => dest.Age, opt => 
                     opt.MapFrom(src => src.DateofBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto,User>();
         }
     }
 }
