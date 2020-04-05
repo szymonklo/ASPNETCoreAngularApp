7dcbe92c8a69e4bc5f2be909d3a9321d314885b5
szymonklo <58526555+szymonklo@users.noreply.github.com>
2020-04-05 22:37:40 +0200

    Likes

--- ASPNETCoreAngular-SPA/src/app/app.module.ts
+++ ASPNETCoreAngular-SPA/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListResolver } from './_resolvers/list.resolver';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -89,6 +90,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       MemberListResolver,
       MemberEditResolver,
       PreventUnsavedChanges,
+      ListResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
    bootstrap: [
--- ASPNETCoreAngular.API/Controllers/UsersController.cs
+++ ASPNETCoreAngular.API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ASPNETCoreAngular.API.Data;
 using ASPNETCoreAngular.API.Dtos;
 using ASPNETCoreAngular.API.Helpers;
+using ASPNETCoreAngular.API.Models;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -72,6 +73,34 @@ namespace ASPNETCoreAngular.API.Controllers
                 return NoContent();
 
             throw new Exception($"Updating user {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if(like != null)
+                return BadRequest("You already like this user");
+
+            if (await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Models.Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if (await _repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like the user");
         }
     }
 }
